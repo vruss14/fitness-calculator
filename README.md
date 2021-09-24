47ab27dcdc18a6579e477f64bf095795bfe958cd
Valerie Russell <vruss14@gmail.com>
2021-09-24 15:10:43 -0400

    refactored code, wrote README, added screenshots

--- README.md
+++ README.md
@@ -2,7 +2,39 @@
 
 ## Overview
 
-A CLI solution built with Python for calculating fitness measurements such as body mass index (BMI), estimated body fat percentage, and resting heart rate.
+A CLI solution built with Python for calculating fitness measurements such as body mass index (BMI), estimated protein intake recommendations, and heart rate.
+
+## Description
+
+This project empowers fitness-conscious users by providing them with relevant data, tailored to their individual needs, in an efficient way (while also preserving their privacy). Knowing calculations such as a recommended protein or water intake can be useful not only for athletes, but for anyone interested in their health and wellness. While the best data comes through consulting with a healthcare provider that knows more information about a person's health, these calculations are an efficient way to get fairly accurate estimates.
+
+Several basic concepts in Python are incorporated within this program, including importing an external module, conditional execution, dictionaries (and accessing values through keys), number and string conversions, and mathematical calculations. I was able to build the program fairly quickly, and appreciated the opportunity to work with several dictionaries. The syntax for dictionaries is slightly different than JavaScript objects, and so I was glad to develop a stronger understanding of how to access values (user inputs) through keys. I also learned how to install an external module, inquirer, rather than rely solely on Python's internal libraries. I chose to import inquirer since it provides a straightforward way to ask for a user's input with a select number of choices (preventing errors/misunderstandings when entering information).
+
+Overall, this was an interesting project to build and I enjoyed learning more about Python fundamentals as well as the strengths and limitations of various modern fitness calculations.
+
+## Technologies Used
+
+- Python
+- Inquirer
+
+## Installation
+
+To run the application, clone the repository and navigate to the correct working directory. Next, run ```python3 index.py``` to start the main menu.
+## Usage
+
+This program involves several simple calculations for health and fitness estimates. It is not intended to diagnose or offer medical advice. For health-related questions and concerns, please contact a local healthcare provider.
+
+**Below is a screenshot CLI's main menu:**
+
+![CLI](./assets/fitness-calc-menu-screenshot.png)
+
+**Below is what the CLI looks like when calculating heart rate:**
+
+![CLI](./assets/fitness-calc-heart-screenshot.png)
+
+## Credits
+
+Valerie Russell was the sole contributor to this project. Contact her at vruss14@gmail.com.
 
 ### References
 
--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import inquirer
 
 def present_main_menu():
+    print('\nPlease keep in mind that this program does not store any of your information and will not ask any personal identification information. The estimates you receive from the calculations may not be entirely accurate. Please consult with a healthcare provider to make informed decisions about your health and wellness.\n')
     main_menu = [
     inquirer.List('calculation_type',
         message="Which fitness calculation would you like to do?",
@@ -24,10 +25,10 @@ def choose_sub_menu(user_choice):
     else:
         run_heart_calculation()
 
-# BMI calculation estimate on the CDC website is height divided by weight squared, multiplied by 703
+# BMI calculation estimate is height divided by weight squared, multiplied by 703
 # The round method rounds the answer to two decimal places
 # The user's BMI value is passed to the check_bmi function, which evaluates what range the BMI falls under.
- 
+
 def run_bmi_calculation():
     bmi_questions = [
     inquirer.Text('height', message="What is your height? Please enter a numeric value in inches."),
@@ -42,13 +43,16 @@ def run_bmi_calculation():
 
 def check_bmi(user_bmi):
     if(user_bmi < 18.5):
-        return('According to the CDC, your BMI falls within the underweight range.')
+        print('According to the CDC, your BMI falls within the underweight range.\n')
     elif(user_bmi > 18.5 and user_bmi < 24.9):
-        return('According to the CDC, your BMI falls within the normal/healthy weight range.')
+        print('According to the CDC, your BMI falls within the normal/healthy weight range.\n')
     elif(user_bmi > 25 and user_bmi < 29.9):
-        return('According to the CDC, your BMI falls within the overweight range.')
+        print('According to the CDC, your BMI falls within the overweight range.\n')
     elif(user_bmi > 30):
-        return('According to the CDC, your BMI falls within the obese range.')
+        print('According to the CDC, your BMI falls within the obese range.\n')
+    new_calculation()
+
+# This calculation first calculates the user's BMR then that value is multipled based on the amount of physical activity the person does
 
 def run_caloric_calculation():
     print("\nThis calculation differs for men and women, since men tend to have higher basal metabolic rates (BMR) than women.")
@@ -66,36 +70,24 @@ def run_caloric_calculation():
         choices=['Sedentary (little to no exercise)', 'Lightly active (light exercise/sports 1–3 days/week)', 'Moderately active (moderate exercise/sports 3-5 days/week', 'Very active (hard exercise/sports 6-7 days/week', 'Extra active (very hard exercise/sports & physical job or 2x training'],
         ),               
     ]
-
     response = inquirer.prompt(caloric_questions)
+
     if(response['body_type'] == 'Men'):
         male_caloric_calculation(response)
     else:
         female_caloric_calculation(response)
 
 def male_caloric_calculation(user_responses):
-
     # Need to calculate BMR first; then the caloric intake depends on how much exercise the person performs on average each day
     user_responses['BMR'] = round(66 + (6.3 * int(user_responses['weight'])) + (12.9 * int(user_responses['height'])) - (6.8 * int(user_responses['age'])), 2)
+    final_caloric_calculation(user_responses)
     
-    if(user_responses['exercise'] == 'Sedentary (little to no exercise)'):
-        user_responses['caloric_need'] = round(int(user_responses['BMR']) * 1.2, 2)
-    elif(user_responses['exercise'] == 'Lightly active (light exercise/sports 1–3 days/week)'):
-        user_responses['caloric_need'] = round(int(user_responses['BMR']) * 1.375, 2)
-    elif(user_responses['exercise'] == 'Moderately active (moderate exercise/sports 3-5 days/week'):
-        user_responses['caloric_need'] = round(int(user_responses['BMR']) * 1.55, 2)
-    elif(user_responses['exercise'] == 'Very active (hard exercise/sports 6-7 days/week'):
-        user_responses['caloric_need'] = round(int(user_responses['BMR']) * 1.725, 2)
-    else:
-        user_responses['caloric_need'] = round(int(user_responses['BMR']) * 1.9, 2)
-
-    print('\nYour estimated daily caloric intake is: '+ str(user_responses['caloric_need']) + ' calories.')
-    print('\nPlease note that this is a mathematical estimate for approximately how many calories are required to maintain your current body weight. The calculated value may not be entirely accurate. Please consult a healthcare provider when considering significant changes to your daily caloric intake.')
-
 def female_caloric_calculation(user_responses):
     # Calculating BMR
     user_responses['BMR'] = round(655 + (4.3 * int(user_responses['weight']) + (4.7 * int(user_responses['height']) - (4.7 * int(user_responses['age'])))), 2)
-    
+    final_caloric_calculation(user_responses)
+
+def final_caloric_calculation(user_responses):
     if(user_responses['exercise'] == 'Sedentary (little to no exercise)'):
         user_responses['caloric_need'] = round(int(user_responses['BMR']) * 1.2, 2)
     elif(user_responses['exercise'] == 'Lightly active (light exercise/sports 1–3 days/week)'):
@@ -106,9 +98,12 @@ def female_caloric_calculation(user_responses):
         user_responses['caloric_need'] = round(int(user_responses['BMR']) * 1.725, 2)
     else:
         user_responses['caloric_need'] = round(int(user_responses['BMR']) * 1.9, 2)
-    
-    print('\nYour estimated daily caloric intake is: '+ str(user_responses['caloric_need']) + ' calories.')
-    print('\nPlease note that this is a mathematical estimate for approximately how many calories are required to maintain your current body weight. The calculated value may not be entirely accurate. Please consult a healthcare provider when considering significant changes to your daily caloric intake.')
+
+    print('\nYour estimated daily caloric intake is: '+ str(user_responses['caloric_need']) + ' calories.\n')
+    new_calculation()
+
+# Amount of water is about half to two-thirds of a person's body weight
+# Calculates a minimum and maximium in ounces, and then turns that into an approxmate range for glasses of water
 
 def run_water_calculation():
     water_question = [inquirer.Text('weight', message="What is your body weight? Please enter a numeric value in pounds.")]
@@ -119,14 +114,15 @@ def run_water_calculation():
     response['max_glasses'] = int(response['max']) / 8
 
     print('\nIt\'s recommended that you drink between ' + str(response['min']) +  
-    ' oz and ' + str(response['max']) + ' oz per day. To put that into perspective, that\'s about ' + 
+    ' ounces and ' + str(response['max']) + ' oz per day. To put that into perspective, that\'s about ' + 
     str(response['min_glasses']) + ' to ' + str(response['max_glasses']) + 
-    ' glasses of water per day.')
+    ' glasses of water per day.\n')
 
-    print('Please note that this is an estimate and may not be entirely accurate.')
+    new_calculation()
 
 # Recommendation is 0.36 - 0.45 grams of protein per pound of body weight; 
 # if exercising, then 0.8 to 1.0 gram per pound of body weight
+
 def run_protein_calculation():
     print('\nYour amount of exercise is a significant factor in determining your recommended daily protein intake.')
     protein_questions = [
@@ -136,7 +132,6 @@ def run_protein_calculation():
         ),
     inquirer.Text('weight', message="What is your body weight? Please enter a numeric value in pounds.")
     ]
-
     response = inquirer.prompt(protein_questions)
 
     if(response['exercise'] == 'Yes'):
@@ -146,15 +141,29 @@ def run_protein_calculation():
         response['min_protein'] = round(int(response['weight']) * 0.36)
         response['max_protein'] = round(int(response['weight']) * 0.45)
     
-    print('\nYour recommended protein intake is: ' + str(response['min_protein']) + ' – ' + str(response['max_protein']) + ' grams.' )
-    print('\nPlease note that this is an estimate and may not be entirely accurate.')
+    print('\nYour recommended protein intake is: ' + str(response['min_protein']) + ' – ' + str(response['max_protein']) + ' grams.\n' )
+    new_calculation()
 
 def run_heart_calculation():
     print('\nPlace two fingers on a place where you can feel your pulse (like your wrist and neck). Count the beats for 30 seconds.')
     heart_question = [inquirer.Text('heartbeats', message="How many heartbeats did you count? Please enter an integer value.")]
     response = inquirer.prompt(heart_question)
     response['rate'] = int(response['heartbeats']) * 2
-    print('Your current heart rate is: ' + str(response['rate']))
-    print('\nPlease note that this is an estimate and may not be entirely accurate.')
+    print('\nYour current heart rate is: ' + str(response['rate']) + '.\n')
+    new_calculation()
+
+def new_calculation():
+    repeat_question = [
+    inquirer.List('repeat',
+        message="\nWould you like to do another calculation?",
+        choices=['Yes', 'No'],
+    )]
+    response = inquirer.prompt(repeat_question)
+
+    if(response['repeat'] == 'Yes'):
+        present_main_menu()
+    else:
+        print('\nThanks for using this application!')
+        return
 
 present_main_menu()
